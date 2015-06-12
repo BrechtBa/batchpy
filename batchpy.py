d879bfa0e9ceeff4676e3cbbec88c65086bed7cc
brecht <brecht.baeten@gmail.com>
2015-06-12 09:15:53 +0200

    added functionality to compare saved runs and new runs for differences

--- batchpy.py
+++ batchpy.py
@@ -15,35 +15,45 @@ class Batch():
 		self.run = []
 		self.resname = []
 		self.res = []
-		self.currentrun = 0
+		self.parname = []
+		self.par = []
+		self.rundone = []
 		
-	def add_run(self,run,res='res'):
+	def add_run(self,run,res='res',par='par'):
 		"""
 		Adds a run
 		
 		Inputs:
 		run: a callable object which runs whatever needs to be run
-		res: a string to a results attribute of the run object
+		res: a string reference to a results dictionary of the run object
+		par: a string reference to a parameters dictionary of the run object used to identify the run
 		
 		Example:
-		batch.add_run(cal,res='results')
+		batch.add_run(cal,res='results',par='temp.parameters')
 		batch()
 		# will run cal() and store cal.results in batch.res[0] after completion
+		# parameters in cal.temp.parameters will be taken to define the run
 		"""
 		
 		self.run.append(run)
 		self.resname.append(res)
-		self.res.append(getattr(self.run[-1],self.resname[-1]))
+		self.res.append(multi_getattr(self.run[-1],self.resname[-1]))
+		self.parname.append(par)
+		self.par.append(multi_getattr(self.run[-1],self.parname[-1]))
+		self.rundone.append(False)
+		
+		# check if there are results for a run with this hash and load it if so
 		self.load(len(self.run)-1)
 		
-	def add_factorial_runs(self,runcreator,inputs,res='res'):
+	def add_factorial_runs(self,runcreator,inputs,res='res',par='par'):
 		"""
 		Adds runs 
 		
 		Arguments:
 		runcreator: function which returns run objects given a set of input parameters
 		inputs: dict with input names and a list of values
-		res: a string to a results attribute of the run object
+		res: a string reference to a results attribute of the run object
+		par: a string reference to a parameters attribute of the run object used to identify the run
 		
 		Example:
 		batch.add_factorial_runs(createcal,{'par1':[0,1,2],'par2':[5.0,7.1]},res='results')
@@ -60,7 +70,7 @@ class Batch():
 		
 		for vals in valslist:
 			input = {key:val for key,val in zip(inputs.keys(),vals)}
-			self.add_run(runcreator(**input),res=res)
+			self.add_run(runcreator(**input),res=res,par=par)
 		
 	def clear_run(self,run):
 		self.currentrun = run
@@ -74,9 +84,9 @@ class Batch():
 
 		filename = self._savepath()
 		
-		np.savez(filename,self.currentrun,self.res)
+		np.savez(filename,self.rundone,self.res,self.resname,self.parname,self.par)
 		
-	def load(self,run):
+	def load(self,idx):
 		"""
 		check is there is a file with the appropriate name in 'curent directory/_res/name.pyz'
 		and loads the data and currentrun index from it
@@ -86,11 +96,18 @@ class Batch():
 		
 		if os.path.isfile(filename):
 			temp = np.load(filename)
-			self.currentrun = temp['arr_0'].item()
-			res = temp['arr_1']
+			rundones = temp['arr_0']
+			ress = temp['arr_1']
+			resnames = temp['arr_2']
+			parnames = temp['arr_3']
+			pars = temp['arr_4']
 			
-			if len(res) > run:
-				self.res[run].update(res[run])
+			for rundone,res,resname,parname,par in zip(rundones,ress,resnames,parnames,pars):
+				if resname == self.resname[idx] and parname == self.parname[idx]:
+					# if the parameter attributes of saved and created runs match update the run
+					if multi_getattr(self.run[idx],parname) == par:
+						self.rundone[idx] = rundone
+						self.res[idx].update(res)
 				
 	def get_res(self,key):
 		"""
@@ -107,17 +124,25 @@ class Batch():
 		"""
 		title_width = 80
 		
+		#check which runs are to be done
+		runs = []
 		if run < 0:
-			runs = range(self.currentrun, len(self.run))
+			for idx in range(len(self.run)):
+				if not self.rundone[idx]:
+					runs.append(idx)
 		else:
 			if isinstance(run,list):
-				runs = run
+				for idx in run:
+					if not self.rundone[idx]:
+						runs.append(idx)
 			else:
-				runs = [run]
+				if not self.rundone[run]:
+					runs.append(run)
+			
 			
 		starttime = time.time()
 		runcount = 0
-		for i in runs:
+		for idx in runs:
 			#print(title_width*'#')
 			runtime = time.time()-starttime
 			if runcount==0:
@@ -127,7 +152,8 @@ class Batch():
 			
 			runcount += 1
 				
-			title_str = '###   run %s / %s' % (self.currentrun,len(runs))
+			title_str = '###   run %s in ' % (idx)
+			title_str += strlist(runs)
 			title_str += '       runtime: %.1f min' % (runtime/60)
 			title_str += '       eta: '+etastr
 			title_str += (title_width-len(title_str)-3)*' ' +'###'
@@ -136,14 +162,14 @@ class Batch():
 			#print(title_width*'#')
 			print(' ')
 			
-			runobj = self.run[i]
+			runobj = self.run[idx]
 			runobj()
 			
 			# update the res attribute
-			self.res[i] = getattr(self.run[i],self.resname[i])
+			self.res[idx] = multi_getattr(self.run[idx],self.resname[idx])
 			
-			if run < 0:
-				self.currentrun = i+1
+			# set the current run to done
+			self.rundone[idx] = True
 			
 			self.save()
 			print(' ')
@@ -156,3 +182,38 @@ class Batch():
 			os.makedirs(dirname)
 			
 		return filename
+		
+		
+		
+# helper functions
+def multi_getattr(obj, attr, default = None):
+	"""
+	Get a named attribute from an object; multi_getattr(x, 'a.b.c.d') is
+	equivalent to x.a.b.c.d. When a default argument is given, it is
+	returned when any attribute in the chain doesn't exist; without
+	it, an exception is raised when a missing attribute is encountered.
+
+	Example:
+	obj  = [1,2,3]
+	attr = "append.__doc__.capitalize.__doc__"
+
+	multi_getattr(obj, attr) #Will return the docstring for the
+							 #capitalize method of the builtin string
+							 #object
+	"""
+	attributes = attr.split(".")
+	for i in attributes:
+		try:
+			obj = getattr(obj, i)
+		except AttributeError:
+			if default:
+				return default
+			else:
+				raise
+	return obj
+        
+def strlist(runs):
+	if len(runs) > 5:
+		return '[' + str(runs[0]) + ',' + str(runs[1]) + '...' + str(runs[-2]) + ',' + str(runs[-1]) +']'
+	else:
+		return str(runs)
