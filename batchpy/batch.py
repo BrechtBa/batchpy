d76fcd93fc779c8172c695ac102aebfce70b60ae
BrechtBa <brecht.baeten@gmail.com>
2016-06-27 09:53:01 +0200

    updated docstrings

--- batchpy/batch.py
+++ batchpy/batch.py
@@ -8,153 +8,153 @@ import time
 
 class Batch(object):
 
-	def __init__(self,name,path='',saveresult=True):
-		"""
-		creates a batch
-		
-		Parameters:
-		name:			string, a name for the batch
-		path:			string, a optional path to store results, if not provided the current path is chosen
-		saveresult: 	boolean, save the results to disk or not, this argument is passed to all runs
-		"""
-		
-		self.name = name
-		self.path = path
-		
-		self.run = []
-		self._saveresult = saveresult
-		
-	def add_run(self,runclass,parameters):
-		"""
-		Adds a run
-		
-		Parameters:
-		runclass: 		a class reference which creates an object when supplied the parameters
-		parameters: 	a dictionary of parameters to be supplied to the init function of the runclass
-		
-		Example:
-		batch.add_run(Cal,{'A':1,'B':[1,2,3],'C':'spam'})
-		batch()
-		# will run cal() and store the return of cal() in batch.res[] after completion
-		# the run will be assigned an id according to cal.id
-		"""
-		
-		self.run.append(runclass(self,saveresult=self._saveresult,**parameters))
-		
-	def add_factorial_runs(self,runclass,parameters):
-		"""
-		Adds runs 
-		
-		Parameters:
-		runcreator: function which returns a run object given a set of input parameters
-		inputs: dict with input names and a list of values
-		
-		Example:
-		batch.add_factorial_runs(createcal,{'par1':[0,1,2],'par2':[5.0,7.1]})
-		# is equivalent with:
-		batch.add_run(createcal(par1=0,par2=5.0))
-		batch.add_run(createcal(par1=0,par2=7.1))
-		batch.add_run(createcal(par1=1,par2=5.0))
-		batch.add_run(createcal(par1=1,par2=7.1))
-		batch.add_run(createcal(par1=2,par2=5.0))
-		batch.add_run(createcal(par1=2,par2=7.1))
-		"""
-		
-		valslist = list(itertools.product(*parameters.values()))
-		
-		for vals in valslist:
-			par = {key:val for key,val in zip(parameters.keys(),vals)}
-			self.add_run( runclass,par )
-			
-		
-	def __call__(self,runind=-1):
-		"""
-		runs the remainder of the batch or a specified run
-		
-		Parameters:
-		runind:			int or list of ints, indices of the runs to be executed, -1 for all runs
-		"""
-		title_width = 80
-		
-		#check which runs are to be done
-		runs = []
-		
-		
-		if isinstance(runind,list):
-			for ind in runind:
-				if not self.run[ind].done:
-					runs.append(ind)
-		else:		
-			if runind < 0:
-				for ind in range(len(self.run)):
-					if not self.run[ind].done:
-						runs.append(ind)
-						
-			elif not self.run[runind].done:
-				runs.append(runind)
-	
+    def __init__(self,name,path='',saveresult=True):
+        """
+        creates a batch
+        
+        Parameters:
+            name:            string, a name for the batch
+            path:            string, a optional path to store results, if not provided the current path is chosen
+            saveresult:      boolean, save the results to disk or not, this argument is passed to all runs
+        """
+        
+        self.name = name
+        self.path = path
+        
+        self.run = []
+        self._saveresult = saveresult
+        
+    def add_run(self,runclass,parameters):
+        """
+        Adds a run
+        
+        Parameters:
+            runclass:        a class reference which creates an object when supplied the parameters
+            parameters:      a dictionary of parameters to be supplied to the init function of the runclass
+        
+        Example:
+            batch.add_run(Cal,{'A':1,'B':[1,2,3],'C':'spam'})
+            batch()
+            # will run cal() and store the return of cal() in batch.res[] after completion
+            # the run will be assigned an id according to cal.id
+        """
+        
+        self.run.append(runclass(self,saveresult=self._saveresult,**parameters))
+        
+    def add_factorial_runs(self,runclass,parameters):
+        """
+        Adds runs 
+        
+        Parameters:
+            runcreator:      function which returns a run object given a set of input parameters
+            inputs:          dict with input names and a list of values
+        
+        Example:
+            batch.add_factorial_runs(createcal,{'par1':[0,1,2],'par2':[5.0,7.1]})
+            # is equivalent with:
+            batch.add_run(createcal(par1=0,par2=5.0))
+            batch.add_run(createcal(par1=0,par2=7.1))
+            batch.add_run(createcal(par1=1,par2=5.0))
+            batch.add_run(createcal(par1=1,par2=7.1))
+            batch.add_run(createcal(par1=2,par2=5.0))
+            batch.add_run(createcal(par1=2,par2=7.1))
+        """
+        
+        valslist = list(itertools.product(*parameters.values()))
+        
+        for vals in valslist:
+            par = {key:val for key,val in zip(parameters.keys(),vals)}
+            self.add_run( runclass,par )
+            
+        
+    def __call__(self,runind=-1):
+        """
+        runs the remainder of the batch or a specified run
+        
+        Parameters:
+            runind:          int or list of ints, indices of the runs to be executed, -1 for all runs
+        """
+        title_width = 80
+        
+        #check which runs are to be done
+        runs = []
+        
+        
+        if isinstance(runind,list):
+            for ind in runind:
+                if not self.run[ind].done:
+                    runs.append(ind)
+        else:        
+            if runind < 0:
+                for ind in range(len(self.run)):
+                    if not self.run[ind].done:
+                        runs.append(ind)
+                        
+            elif not self.run[runind].done:
+                runs.append(runind)
+    
 
-		skip = int( np.ceil( len(runs)/50. ) )
-			
-		starttime = time.time()
-		for i,run in enumerate(runs):
-			
-			# print the run title string
-			if i%skip==0:
-				runtime = time.time()-starttime
-				if i==0:
-					etastr = '/'
-				else:
-					etastr = '{0:.1f} min'.format( runtime/i*(len(runs)-i)/60 )
-					
-				title_str = '### run {0} in '.format(run)
-				title_str += strlist(runs)
-				title_str += (40-len(title_str))*' '
-				title_str += 'runtime: {0:.1f} min'.format(runtime/60)
-				title_str += 4*' '
-				title_str += 'eta: '+etastr
-				title_str += (title_width-len(title_str)-3)*' ' +'###'
-				print(title_str)
-				
-				# flush the printing cue
-				sys.stdout.flush()
+        skip = int( np.ceil( len(runs)/50. ) )
+            
+        starttime = time.time()
+        for i,run in enumerate(runs):
+            
+            # print the run title string
+            if i%skip==0:
+                runtime = time.time()-starttime
+                if i==0:
+                    etastr = '/'
+                else:
+                    etastr = '{0:.1f} min'.format( runtime/i*(len(runs)-i)/60 )
+                    
+                title_str = '### run {0} in '.format(run)
+                title_str += strlist(runs)
+                title_str += (40-len(title_str))*' '
+                title_str += 'runtime: {0:.1f} min'.format(runtime/60)
+                title_str += 4*' '
+                title_str += 'eta: '+etastr
+                title_str += (title_width-len(title_str)-3)*' ' +'###'
+                print(title_str)
+                
+                # flush the printing cue
+                sys.stdout.flush()
 
-			# run the run
-			self.run[run]()
-		
-		runtime = time.time()-starttime
-		print('total runtime {0:.1f} min'.format(runtime/60))
-		print('done')
-		sys.stdout.flush()
-		
-	def savepath(self):
-		"""
-		Returns the path where files are saved
-		"""
-		dirname = os.path.join(self.path, '_res' )
-		filename = os.path.join(dirname , self.name )
-		
-		if not os.path.isdir(dirname):
-			os.makedirs(dirname)
-		
-		return dirname
-	
-	def _get_filenames(self):
-		"""
-		Returns a list of found files which correspond to the batch
-		"""
-		
-		dirname = self.savepath()
-		filenames = []
-		files = [f for f in os.listdir(dirname) if re.match(self.name+r'_run.*\.npy', f)]
-		for f in files:
-			filenames.append( os.path.join(dirname , f) )
-				
-		return filenames
-	
+            # run the run
+            self.run[run]()
+        
+        runtime = time.time()-starttime
+        print('total runtime {0:.1f} min'.format(runtime/60))
+        print('done')
+        sys.stdout.flush()
+        
+    def savepath(self):
+        """
+        Returns the path where files are saved
+        """
+        dirname = os.path.join(self.path, '_res' )
+        filename = os.path.join(dirname , self.name )
+        
+        if not os.path.isdir(dirname):
+            os.makedirs(dirname)
+        
+        return dirname
+    
+    def _get_filenames(self):
+        """
+        Returns a list of found files which correspond to the batch
+        """
+        
+        dirname = self.savepath()
+        filenames = []
+        files = [f for f in os.listdir(dirname) if re.match(self.name+r'_run.*\.npy', f)]
+        for f in files:
+            filenames.append( os.path.join(dirname , f) )
+                
+        return filenames
+    
 # helper functions
 def strlist(runs):
-	if len(runs) > 5:
-		return '[' + str(runs[0]) + ',' + str(runs[1]) + ',...,' + str(runs[-2]) + ',' + str(runs[-1]) +']'
-	else:
-		return str(runs)
+    if len(runs) > 5:
+        return '[' + str(runs[0]) + ',' + str(runs[1]) + ',...,' + str(runs[-2]) + ',' + str(runs[-1]) +']'
+    else:
+        return str(runs)
--- batchpy/run.py
+++ batchpy/run.py
@@ -7,120 +7,120 @@ import inspect
 
 
 class Run(object):
-	def __init__(self,batch,saveresult=True,**parameters):
-		"""
-		creates a run
-		
-		Arguments:
-		batch:		batch, the batch the run belongs to
-		saveresult: boolean, save the results to disk or not, if not the result is available in the _result attribute
-		parameters: other keyword parameters
-		"""
-		
-		self.batch = batch
-		self._saveresult = saveresult
-		self._result = None
-		
-		# get the parameters from the run function
-		self.parameters = {}
-		a = inspect.getargspec(self.run)
-		for key,val in zip(a.args[-len(a.defaults):],a.defaults):
-			self.parameters[key] = val
-		
-		for key,val in parameters.iteritems():
-			self.parameters[key] = val
-		
-		
-		# create the run id
-		self.id = self.set_id(self.parameters)
-		
-		self.check_result()
+    def __init__(self,batch,saveresult=True,**parameters):
+        """
+        creates a run
+        
+        Arguments:
+            batch:        batch, the batch the run belongs to
+            saveresult: boolean, save the results to disk or not, if not the result is available in the _result attribute
+            parameters: other keyword parameters
+        """
+        
+        self.batch = batch
+        self._saveresult = saveresult
+        self._result = None
+        
+        # get the parameters from the run function
+        self.parameters = {}
+        a = inspect.getargspec(self.run)
+        for key,val in zip(a.args[-len(a.defaults):],a.defaults):
+            self.parameters[key] = val
+        
+        for key,val in parameters.iteritems():
+            self.parameters[key] = val
+        
+        
+        # create the run id
+        self.id = self.set_id(self.parameters)
+        
+        self.check_result()
 
-		
-	def run(self,**parameters):
-		"""
-		redefine this method in a child class to define the run
-		"""
-		return {}
-		
-	def __call__(self):
-		if not self.done:
-			res = self.run(**self.parameters)
-			if self._saveresult:
-				np.save(self._filename(),{'res':res,'id':self.id})
-			else:
-				self._result = res
-			
-			self.done = True
-		else:
-			if self._saveresult:
-				res = self.load()
-			else:
-				res = self._result
-		return res
-		
-	def _filename(self):
-		return os.path.join(self.batch.savepath() , '{}_{}.npy'.format(self.batch.name,self.id))
-		
-	def load(self):
-		"""
-		tries to return the run results
-		"""
-		
-		try:
-			if self._saveresult:
-				data = np.load(self._filename())
-				res = data.item()['res']
-				return res
-			else:
-				return self._result
-		except:
-			return None
-			
-	def clear(self):
-		"""
-		tries to delete the result file from the disk
-		"""
-		try:
-			os.remove(self._filename())
-			self.done = False
-			return True
-		except:
-			return False
-			
-	def set_id(self,parameters):
-		"""
-		function creates an id hash from the parameters
-		"""
-		
-		id_dict = {}
-		for key in parameters.keys():
-			c0 = isinstance(parameters[key],types.BooleanType)
-			c1 = isinstance(parameters[key],types.IntType)
-			c2 = isinstance(parameters[key],types.LongType)
-			c3 = isinstance(parameters[key],types.FloatType)
-			c4 = isinstance(parameters[key],types.ComplexType)
-			c5 = isinstance(parameters[key],types.StringType)
-			c6 = isinstance(parameters[key],types.UnicodeType)
-			c7 = isinstance(parameters[key],types.TupleType)
-			c8 = isinstance(parameters[key],types.ListType)
-			c9 = isinstance(parameters[key],types.DictType)
-			
-			if c1 or c2 or c3 or c4 or c5 or c6 or c7 or c8 or c9:
-				id_dict[key] = parameters[key]
-				
-			elif isinstance(parameters[key],types.FunctionType):
-				id_dict[key] = parameters[key].__name__				
-			elif isinstance(parameters[key],types.ClassType):
-				id_dict[key] = parameters[key].__name__
-			elif isinstance(parameters[key],types.MethodType):
-				id_dict[key] = parameters[key].__name__
+        
+    def run(self,**parameters):
+        """
+        redefine this method in a child class to define the run
+        """
+        return {}
+        
+    def __call__(self):
+        if not self.done:
+            res = self.run(**self.parameters)
+            if self._saveresult:
+                np.save(self._filename(),{'res':res,'id':self.id})
+            else:
+                self._result = res
+            
+            self.done = True
+        else:
+            if self._saveresult:
+                res = self.load()
+            else:
+                res = self._result
+        return res
+        
+    def _filename(self):
+        return os.path.join(self.batch.savepath() , '{}_{}.npy'.format(self.batch.name,self.id))
+        
+    def load(self):
+        """
+        tries to return the run results
+        """
+        
+        try:
+            if self._saveresult:
+                data = np.load(self._filename())
+                res = data.item()['res']
+                return res
+            else:
+                return self._result
+        except:
+            return None
+            
+    def clear(self):
+        """
+        tries to delete the result file from the disk
+        """
+        try:
+            os.remove(self._filename())
+            self.done = False
+            return True
+        except:
+            return False
+            
+    def set_id(self,parameters):
+        """
+        function creates an id hash from the parameters
+        """
+        
+        id_dict = {}
+        for key in parameters.keys():
+            c0 = isinstance(parameters[key],types.BooleanType)
+            c1 = isinstance(parameters[key],types.IntType)
+            c2 = isinstance(parameters[key],types.LongType)
+            c3 = isinstance(parameters[key],types.FloatType)
+            c4 = isinstance(parameters[key],types.ComplexType)
+            c5 = isinstance(parameters[key],types.StringType)
+            c6 = isinstance(parameters[key],types.UnicodeType)
+            c7 = isinstance(parameters[key],types.TupleType)
+            c8 = isinstance(parameters[key],types.ListType)
+            c9 = isinstance(parameters[key],types.DictType)
+            
+            if c1 or c2 or c3 or c4 or c5 or c6 or c7 or c8 or c9:
+                id_dict[key] = parameters[key]
+                
+            elif isinstance(parameters[key],types.FunctionType):
+                id_dict[key] = parameters[key].__name__                
+            elif isinstance(parameters[key],types.ClassType):
+                id_dict[key] = parameters[key].__name__
+            elif isinstance(parameters[key],types.MethodType):
+                id_dict[key] = parameters[key].__name__
 
-		return hashlib.sha1(str([ id_dict[key] for key in id_dict.keys() ])).hexdigest()
-	
-	def check_result(self):
-		# check if there are results saved with the same id
-		if os.path.isfile(self._filename()):
-			self.done = True
-		else:
-			self.done = False
+        return hashlib.sha1(str([ id_dict[key] for key in id_dict.keys() ])).hexdigest()
+    
+    def check_result(self):
+        # check if there are results saved with the same id
+        if os.path.isfile(self._filename()):
+            self.done = True
+        else:
+            self.done = False
